bb0563e35cbae440989bf8ea89050af585e8cff4
Candace Williams <candace.williams94@hotmail.com>
2021-10-19 17:09:41 -0400

    add up minor edits.

---
+++ SqlServerEFSample/Chore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerEFSample
+{
+    public class Chore
+    {
+        public int ChoreId { get; set; }
+        public string ChoreName { get; set; }
+        public string ChoreAssignment { get; set; }
+        public bool IsComplete { get; set; }
+        public virtual User AssignedTo { get; set; }
+
+        public override string ToString()
+        {
+            return "Chore [id=" + this.ChoreId + ", name=" + this.ChoreName + "assignment=" + this.ChoreAssignment + "IsComplete" + this.IsComplete + "]";
+        }
+    }
+}
